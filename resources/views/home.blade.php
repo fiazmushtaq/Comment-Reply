8ce04f2b9227cf8753dca399adeb85dbc79f2a32
Fiaz Mushtaq <fiazmushtaq.dev@gmail.com>
2025-07-28 20:18:06 +0500

    Heading h3 Color modification

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <h3
-    style="background-color: rgb(2, 72, 24); color:rgb(255, 255, 255); text-align: center; margin:-0.4px 0;border-radius: 0 0 50% 50% / 0 0 15px 15px;font-size: 30px;">
+    style="background-color: rgba(1, 43, 63, 1); color:rgb(255, 255, 255); text-align: center; margin:-0.4px 0;border-radius: 0 0 50% 50% / 0 0 15px 15px;font-size: 30px;">
     CodeX Computer Zone</h3>
 
     <!-- Comment section -->
